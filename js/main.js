56a736a5436e4c67957630d4d3f992caa3a1ef9c
satishkhanal76 <satishkhanal76@gmail.com>
2025-04-25 01:18:17 -0400

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
-const markerSvg = `<svg class="svg-icon" viewBox="-4 0 36 36">
-    <path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
-    <circle fill="black" cx="14" cy="14" r="7"></circle>
-  </svg>`;
+import Globe from "globe.gl";
+
+
+const allMarkers = [];
 const globeContainer = document.getElementById("globe-container");
-const myGlobe = Globe({
-  animateIn: false,
-});
-let globe = myGlobe(globeContainer).globeImageUrl(
-  "//unpkg.com/three-globe/example/img/earth-night.jpg"
+
+const myGlobe = new Globe(globeContainer);
+myGlobe.globeImageUrl(
+  "/earth_day.jpg"
 );
 
+
 myGlobe.onGlobeReady(() => {
-  myGlobe.enablePointerInteraction(false);
+  myGlobe.enablePointerInteraction(true);
   const controls = myGlobe.controls();
-  // controls.autoRotate = true;
 
   controls.enabled = false;
-  console.log(myGlobe.controls());
 
   setTimeout(() => {
     let data = {
@@ -24,10 +22,13 @@ myGlobe.onGlobeReady(() => {
       lng: 86.925,
       altitude: 1,
     };
+    addMarker(27.1751, 78.0421, "red"); // Mount Everest
+
 
     myGlobe.pointOfView(data, 1000);
 
     setTimeout(() => {
+
       myGlobe.pointOfView(
         {
           lat: 10.9881,
@@ -39,63 +40,23 @@ myGlobe.onGlobeReady(() => {
       controls.autoRotate = true;
     }, 5000);
   }, 1000);
-
-  //   setTimeout(() => {
-  //     myGlobe.pointOfView(
-  //       {
-  //         lat: 27.7172,
-  //         lng: 85.324,
-  //         altitude: 1.5,
-  //       },
-  //       2000
-  //     );
-  //   }, 1000);
-
-  //   setTimeout(() => {
-  //     let data = {
-  //       lat: 27.7172,
-  //       lng: -85.324,
-  //       altitude: 2,
-  //     };
-  //     myGlobe.pointOfView(data, 1000);
-  //     myGlobe.htmlElementsData([data]).htmlElement((d) => {
-  //       const el = document.createElement("div");
-  //       el.innerHTML = markerSvg;
-  //       el.style.color = d.color;
-
-  //       el.onclick = () => console.info(d);
-  //       return el;
-  //     });
-  //     // addPoints(20);
-  //   }, 3000);
-
-  //   setTimeout(() => {
-  //     let data = {
-  //       lat: -30,
-  //       lng: 45,
-  //       altitude: 3,
-  //     };
-  //     myGlobe.htmlElementsData([data]);
-  //     myGlobe.pointOfView(data, 3000);
-  //     // controls.autoRotate = true;
-  //     // controls.autoRotateSpeed = -5;
-  //   }, 5000);
 });
 
-const addPoints = (N) => {
-  const gData = [...Array(N).keys()].map(() => ({
-    lat: (Math.random() - 0.5) * 180,
-    lng: (Math.random() - 0.5) * 360,
-    size: 7 + Math.random() * 30,
-    color: ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
-  }));
 
-  myGlobe.htmlElementsData(gData).htmlElement((d) => {
-    const el = document.createElement("div");
-    el.innerHTML = markerSvg;
-    el.style.color = d.color;
-
-    el.onclick = () => console.info(d);
-    return el;
+const addMarker = (lat, lng, color = "red") => {
+  allMarkers.push({
+    lat,
+    lng,
+    altitude: 0.05,
+    color,
   });
+  myGlobe.pointColor((points) => {
+    return points.color;
+  });
+
+  myGlobe.pointAltitude((points) => {
+    return points.altitude;
+  }
+  );
+  myGlobe.pointsData(allMarkers);
 };
---
+++ vite.config.js
@@ -0,0 +1,3 @@
+export default {
+    base: '/Travel-Agency-Website/',
+}
