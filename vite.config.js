d333c159054339b333182a912325a6c2023a3e10
satishkhanal76 <satishkhanal76@gmail.com>
2026-02-05 17:04:28 -0500

    404 page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from "path";
 
 export default defineConfig({
   base: '/Travel-Agency-Website/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        notfound: resolve(__dirname, "404.html"),
+      },
+    },
+  },
 });
